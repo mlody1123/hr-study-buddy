2e0a4c373a95bc8f44f33da0afb447624c896b9a
Adrian Bujok <adrianbujok93@gmail.com>
2021-03-05 18:56:47 +0100

    Add user list styling

---
+++ src/assets/styles/GlobalStyle.tsx
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+
+
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *::after, *::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'Montserrat', sens-serif;
+  }
+
+  a, button {
+    font-family: 'Montserrat', sens-serif;
+  }
+
+`;
---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,19 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    white: '#FFFFFF',
+    darkGray: '#737C8E',
+    lightGrey: '#F7F8FA',
+    grey: '#C0C7D6',
+    black: '#111111',
+    success: '#8FCB81',
+    error: '#CB8581',
+    warning: '#E1D888',
+  },
+  fontSize: {
+    l: '17px',
+    m: '12px',
+    s: '11px',
+  },
+};
